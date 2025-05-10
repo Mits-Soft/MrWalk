be7b1dcfdb05833d00ee8e89618ecd40223732dc
Mits-Soft <mits.soft.main@gmail.com>
2025-05-10 15:13:40 +0200

    MrWalk: Cleaning

---
+++ __init__.py
@@ -0,0 +1,12 @@
+from .mrwalk import MrWalk
+
+"""
+MrWAlk.
+
+A Python library that allows retrieving a directory info recursively
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Marco Antonio Calviño Coira'
+__credits__ = '2025 MitsSoft'
+
